c254de503dc8dd7938319f5f52da41b8010d708d
itzani18 <98737694+itzani18@users.noreply.github.com>
2025-07-04 13:09:09 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -101,7 +101,7 @@ const Hero = () => {
             </motion.button>
 
             <motion.a
-              href="https://drive.google.com/file/d/14WvddZCXvu2J0_GYiXfVAIF3kYdUTd_Z/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1Uk6Q23qry8K3EaxD0G9rCB5AxWheGtXz/view?usp=sharing"
               download
               className="px-6 py-3 border-2 border-cyan-400 text-cyan-400 font-bold rounded hover:bg-cyan-400 hover:text-black transition-all duration-300 flex items-center justify-center"
               whileHover={{ scale: 1.05 }}
